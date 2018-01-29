eb03f851ffa0f868ce13975f5e906e3dd59e07a6
Raghu K <raghuk.vit@gmail.com>
2018-01-29 02:42:05 +0000

    Updating shell_scripts

--- stack.sh
+++ stack.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
 
-echo "Installing Web Server"
+##### Functions
+WEB_F() {
+	echo "Installing Web Server" 
+}
 
-echo "Installing APP Server"
+APP_F() {
+	echo "Installing APP Server"
+}
 
-echo "Installing DB Server"
+DB_F() {
+	echo "Installing DB Server"
+}
+
+ALL_F() {
+	WEB_F
+	APP_F
+	DB_F
+}
+
+##### Main Script
+if [ "$USER" != root ]; then 
+	echo "You should be a root user to perform this script"
+	exit 1
+fi
+
+read -p 'Which Service you would like to install[WEB|APP|DB|ALL] : ' SETUP
+if [ -z "$SETUP" ]; then 
+	SETUP=ALL
+fi
+
+
+case $SETUP in 
+	WEB|web) 
+		WEB_F
+		;;
+	APP|app)
+		APP_F
+		;;
+	DB|db)
+		DB_F
+		;;
+	ALL|all)
+		ALL_F
+		;;
+	*) 
+		echo "Allowed values are WEB|APP|DB|ALL ... Try Again .."
+		exit 1
+esac
