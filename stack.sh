a4f67172b792dba2b70003878847f1e14fc2ba11
Raghu K <raghuk.vit@gmail.com>
2018-01-31 02:34:34 +0000

    Updating shell_scripts

--- stack.sh
+++ stack.sh
@@ -163,6 +163,23 @@ DB_F() {
 	systemctl enable mariadb &>/dev/null
 	systemctl restart mariadb &>/dev/null
 	Stat $?
+
+	Print "Configuring DB"
+	echo "> create database studentapp;
+use studentapp;
+CREATE TABLE Students(student_id INT NOT NULL AUTO_INCREMENT,
+	student_name VARCHAR(100) NOT NULL,
+  student_addr VARCHAR(100) NOT NULL,
+	student_age VARCHAR(3) NOT NULL,
+	student_qual VARCHAR(20) NOT NULL,
+	student_percent VARCHAR(10) NOT NULL,
+	student_year_passed VARCHAR(10) NOT NULL,
+	PRIMARY KEY (student_id)
+);
+grant all privileges on studentapp.* to 'student'@'%' identified by 'student@1';
+flush privileges;" >/tmp/student.sql 
+	mysql < /tmp/student.sql >/tmp/mysql.log 
+	Stat $?
 }
 
 ALL_F() {
